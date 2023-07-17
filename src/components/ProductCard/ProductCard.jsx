a951af60af4af252aca0fb31fb10f8715a319849
HEAD_0223 <andrei.dolgopol2000@gmail.com>
2023-07-17 16:34:42 +0300

    init

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
 		flexDirection: 'column',
 		alignItems: 'center',
 		height: '100%',
-		backgroundColor: theme.palette.bg_color.main,
+		backgroundColor: theme.palette.hint_color.main,
 		color: theme.palette.text_color.main,
 	},
 	productImageContainer: {
