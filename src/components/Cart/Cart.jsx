0d5ffab29406f0da85531257fa9001a4e4fbbee7
HEAD_0223 <andrei.dolgopol2000@gmail.com>
2023-07-19 14:57:16 +0300

    init

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Cart = () => {
 	const location = useLocation();
 	const { cart } = location.state;
+	const navigate = useNavigate();
+
+	const handleEditClick = () => {
+		navigate('/');
+	};
 
 	return (
 		<div>
@@ -17,6 +22,7 @@ export const Cart = () => {
 			))}
 
 			{/* Add your comment input here */}
+			<button onClick={handleEditClick}>Edit</button>
 		</div>
 	);
 };
