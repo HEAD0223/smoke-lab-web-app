25783cf840df04ab4f02e4fe31f8ab2505ebd8a4
HEAD_0223 <andrei.dolgopol2000@gmail.com>
2023-07-20 15:00:11 +0300

    init

--- src/components/ProductModal/ProductModal.jsx
+++ src/components/ProductModal/ProductModal.jsx
@@ -39,11 +39,14 @@ export const ProductModal = ({
 }) => {
 	const classes = useStyles();
 
+	// Decode the base64 image data and create a data URL
+	const imageSrc = url ? `data:image/png;base64,${url}` : 'https://source.unsplash.com/random';
+
 	return (
 		<Modal open={open} onClose={onClose} className={classes.modalContainer}>
 			<Paper className={classes.modalContent}>
 				<Box display="flex" flexDirection="column" alignItems="center">
-					<img src={url} alt={name} className={classes.productImage} />
+					<img src={imageSrc} alt={name} className={classes.productImage} />
 					<Typography variant="h6">{name}</Typography>
 					<Typography variant="subtitle1">Price: {price}</Typography>
 					<Typography variant="subtitle1">SKU: {code}</Typography>
