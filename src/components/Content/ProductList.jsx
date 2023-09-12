e4d9debebedffb7427b33ddb5e22283e444721ad
HEAD_0223 <andrei.dolgopol2000@gmail.com>
2023-09-13 00:10:38 +0300

    init

--- src/components/Content/ProductList.jsx
+++ src/components/Content/ProductList.jsx
@@ -116,7 +116,10 @@ export const ProductList = () => {
 	return (
 		<>
 			{isProductsLoading && (
-				<LinearProgress style={{ backgroundColor: 'var(--tg-theme-hint-color)' }} />
+				<LinearProgress
+					sx={{ color: 'var(--tg-theme-button-color)' }}
+					style={{ backgroundColor: 'var(--tg-theme-hint-color)' }}
+				/>
 			)}
 			<div style={{ display: 'flex', justifyContent: 'center', marginTop: 32 }}>
 				{/* Pass handleManufacturerSelection as a callback to Filter component */}
